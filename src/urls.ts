85f4515f414aaba244caa2ac9473562897f02a4b
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-09-30 15:38:31 -0600

    Add "Stay" by Delta Heavy & Dirty Audio remixed by Maduk

--- src/urls.ts
+++ src/urls.ts
@@ -285,3 +285,4 @@ export const imHere = 'xG8--_OOWE0';
 export const differentStory = 'zo7a_yK_rtw';
 export const setMeFree = 'XvEtJ6zbzyw';
 export const makeItThere = 'Nc6MDm89By0';
+export const stay = '7sbw__MsJZ0';
