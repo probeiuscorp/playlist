b92f14007fe6993fa3d03534c631d751da849918
Caleb Stimpson <caleb.stimpson@gmail.com>
2025-04-16 09:19:25 -0600

    Add 'Sector 1 (SRX)' from Metroid Fusion

--- src/urls.ts
+++ src/urls.ts
@@ -53,6 +53,7 @@ export const ff6Cyan = '-ol1qnMdapY';
 export const ff6Shadow = 'vdBzHamb2v0';
 export const ff6Relm = 'nPJS_-OJMMk';
 export const superMetroid = 'Xpb1hpJrYXM?start=10&end=215';
+export const metroidFusionSRX = 'N2hgVSJ7VBs';
 export const technoMetroid = 'dkDR5OckDfA';
 export const kraid = 'BZeGC7e9M_w';
 export const whatLanaSays = 'mJdwDjUqAMQ';
