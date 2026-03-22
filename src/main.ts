e2e7a5fe54fe05641c985ee2e71e58c450812cf1
Caleb Stimpson <caleb.stimpson@gmail.com>
2026-03-21 21:14:42 -0600

    Fix typo labes -> labels

--- src/main.ts
+++ src/main.ts
@@ -121,7 +121,7 @@ const weighAgainstRepeats = ({ nUnplayable, nHalfway }: RepeatsWeighInit): State
 });
 const repeatsWeigher = weighAgainstRepeats({ nUnplayable: 18, nHalfway: 24 });
 const standardWeighers: Weigher[] = [
-  ({ labels }) => labes.has('ff') ? 1.5 : labels.has('f') ? 1.2 : labels.has('m') ? 0.5 : 1,
+  ({ labels }) => labels.has('ff') ? 1.5 : labels.has('f') ? 1.2 : labels.has('m') ? 0.5 : 1,
   ({ labels }) => (labels.has('stirling') && labels.has('instrumental')) ? 1.25 : 1,
   ({ labels }) => labels.has('silksong') ? 2 : labels.has('hollowknight') ? 1.5 : 1,
   ({ labels }) => labels.has('celeste') ? 1.75 : 1,
