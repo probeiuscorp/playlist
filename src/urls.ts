4f7d0190eb599593fb8ce10ebf89ea7990982e89
Caleb Stimpson <caleb.stimpson@gmail.com>
2026-07-04 10:52:09 -0600

    Add "Heart of the Cards" from Celeste Strawberry Jam

--- src/urls.ts
+++ src/urls.ts
@@ -663,6 +663,10 @@ export const lowGBotany = s('WLbYaIsC9qA', {
   l: 'instrumental strawberry-jam',
   i: -2, s: 2,
 });
+export const belatedValentines = s('fhzhSHEXxrE', {
+  l: 'instrumental strawberry-jam',
+  s: 2, m: 3,
+});
 export const cityOfTears = s('5oj_1wAETSA?end=833', {
   l: 'instrumental hollowknight',
   s: 4, i: -2, g: 3, m: 8,
